2fcba6d7b2c721c45a7d56ad8df49d754fa4a716
adetoro ezekiel <adetoroe787@gmail.com>
2022-08-02 14:58:14 -0400

    created helper function

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const fs = require('fs')
 const path = require('path')
 
 let cache = {} // cache object is where the contents of cached files are stored
+
+function send404(response){
+    response.writeHead(404, {'content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents){
+    response.writeHead(
+        200, {
+            'content-Type': mime.lookup(path.basename(filePath))
+        }
+    );
+    response.end(fileContents)
+}
