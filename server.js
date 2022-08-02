61e2fd4a33395d155ab25fcda4e0b9c62a99a0e4
adetoro ezekiel <adetoroe787@gmail.com>
2022-08-02 15:22:42 -0400

    Server static files

--- server.js
+++ server.js
@@ -1,21 +1,46 @@
-const http = require('http');
-const mime = require('mime')
-const fs = require('fs')
-const path = require('path')
+const http = require("http");
+const mime = require("mime");
+const fs = require("fs");
+const path = require("path");
 
-let cache = {} // cache object is where the contents of cached files are stored
+let cache = {}; // cache object is where the contents of cached files are stored
 
-function send404(response){
-    response.writeHead(404, {'content-Type': 'text/plain'});
-    response.write('Error 404: resource not found.');
-    response.end();
+// 404 helper function
+function send404(response) {
+  response.writeHead(404, { "content-Type": "text/plain" });
+  response.write("Error 404: resource not found.");
+  response.end();
 }
 
-function sendFile(response, filePath, fileContents){
-    response.writeHead(
-        200, {
-            'content-Type': mime.lookup(path.basename(filePath))
-        }
-    );
-    response.end(fileContents)
+// Sendfile helper function
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(200, {
+    "content-Type": mime.lookup(path.basename(filePath)),
+  });
+  response.end(fileContents);
+}
+
+// Server Static files
+function serveStatic(response, cache, absPath) {
+  if (cache[absPath]) {
+    // Check if file is cached in memory
+    sendFile(response, absPath, cache[absPath]); // Serve file from memory
+  } else {
+    fs.exists(absPath, function (exists) {
+      if (exists) {
+        // Check if file exists
+        fs.readFile(absPath, function (err, data) {
+          // Read file from disk
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data); // Server file read from disk
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
 }
